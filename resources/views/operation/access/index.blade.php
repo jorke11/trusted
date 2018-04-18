9a6a15bfafb83ea24f1442f1b65459db256ef0ee
jorge pinedo <jpinedom@hotmail.com>
2018-04-18 11:37:42 -0500

    add

--- resources/views/operation/access/index.blade.php
+++ resources/views/operation/access/index.blade.php
@@ -7,8 +7,8 @@
         <!-- Nav tabs -->
         <ul class="nav nav-tabs" role="tablist" id='myTabs'>
             <li role="presentation"  class="active" id="tabUplod"><a href="#access_control" aria-controls="special" role="tab" data-toggle="tab">Control Acceso</a></li>
-            <li role="presentation" id="tabReception" ><a href="#reception" aria-controls="special" role="tab" data-toggle="tab">Recepcion de Documentos</a></li>
-            <li role="presentation" id="tabAuth"><a href="#authotization" aria-controls="special" role="tab" data-toggle="tab">Autorizaciones</a></li>
+            <!--<li role="presentation" id="tabReception" ><a href="#reception" aria-controls="special" role="tab" data-toggle="tab">Recepcion de Documentos</a></li>-->
+            <!--<li role="presentation" id="tabAuth"><a href="#authotization" aria-controls="special" role="tab" data-toggle="tab">Autorizaciones</a></li>-->
         </ul>
 
         <!-- Tab panes -->
