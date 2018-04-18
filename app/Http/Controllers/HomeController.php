ffb6524533847e11f10164116decf68c1d0eb29c
jorge pinedo <jpinedom@hotmail.com>
2018-04-18 15:24:09 -0500

    add new field

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,8 +19,7 @@ class HomeController extends Controller {
      */
     public function index() {
 
-        if (Auth::user()->role_id != 1) {
-            dd(Auth::user());
+        if (Auth::user()->role_id == 1) {
             $title_db = \App\Models\Administration\Parameters::where("group", "main_title")->first();
             $title = "Trusted";
             if ($title_db != null) {
